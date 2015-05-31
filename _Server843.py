5664e4aa6c4cd10cbe50fc267f14c746d306b8dc
Alex Shao <shao_kai@qq.com>
2015-05-31 11:54:20 +0800

    update code

--- _Server843.py
+++ _Server843.py
@@ -3,16 +3,15 @@ from snjdck import *
 
 import Config
 
+POLICY_FILE = b'<cross-domain-policy><allow-access-from domain="*" to-ports="*" /></cross-domain-policy>'
+
 class Server843Client(Client):
-	def onRecv(self):
-		sock, address = self.sock.accept()
-		buff = sock.recv(1024)
-		print(address, buff)
-		sock.sendall(b'<cross-domain-policy><allow-access-from domain="*" to-ports="*" /></cross-domain-policy>')
-		sock.close()
+	def onRecv(self, context):
+		data = self.sock.recv(64)
+		self.sock.sendall(POLICY_FILE)
+		self.onClose()
 
 
 clientMgr = ClientManager()
 clientMgr.regServer(Config.IP_843, 843, Server843Client)
 clientMgr.run()
-	
