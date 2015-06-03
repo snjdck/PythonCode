f0703c534fadadda6ee804059bd6ddf4f9976579
Alex Shao <shao_kai@qq.com>
2015-06-03 15:32:29 +0800

    update code

--- logic_handlers.py
+++ logic_handlers.py
@@ -2,17 +2,35 @@
 from handler_mgr import *
 import MsgDef
 
+clientCount = 0
+
 @bindMsg(MsgDef.ID_CONNECT)
 def _onConnect(self, fileno, packet):
 	self.clientDict[fileno] = None
 	print("client connect")
+	global clientCount
+	clientCount += 1
+	if 2 == clientCount:
+		msgData = {"first":True}
+		for client_fileno in self.clientDict:
+			self.sendTo(client_fileno, packet.encode(MsgDef.ID_BEGIN_CHESS, msgData))
+			msgData["first"] = False
 
 @bindMsg(MsgDef.ID_DISCONNECT)
 def _onDisConnect(self, fileno, packet):
 	del self.clientDict[fileno]
 	print("client disconnect")
+	global clientCount
+	clientCount -= 1
 
 @bindMsg(MsgDef.ID_CHAT)
 def _onChat(self, fileno, packet):
 	for client_fileno in self.clientDict:
+		self.sendTo(client_fileno, packet.tobytes())
+
+@bindMsg(MsgDef.ID_ACTION)
+def _onAction(self, fileno, packet):
+	for client_fileno in self.clientDict:
+		if fileno == client_fileno:
+			continue
 		self.sendTo(client_fileno, packet.tobytes())
